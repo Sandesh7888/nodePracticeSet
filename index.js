dde620ed3142bef23edd030aa956b37049c14ce1
sandeshkarad7472@gmail.com <sandeshkarad7472@gmail.com>
2025-12-23 22:43:07 +0530

    new

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const FILES_DIR = path.join(__dirname, 'files');
+
+/* Middleware */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* View Engine */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+/* Routes */
+
+// Home – list todos
+app.get('/', (req, res) => {
+    fs.readdir(FILES_DIR, (err, files) => {
+        if (err) {
+            return res.status(500).send('Unable to read files');
+        }
+        res.render('index', { files });
+    });
+});
+
+// Create todo
+app.post('/create', (req, res) => {
+    const { title, description } = req.body;
+
+    if (!title) {
+        return res.status(400).send('Title is required');
+    }
+
+    const fileName = `${title.split(' ').join('_')}.txt`;
+    const content = `Title: ${title}\n\nDescription: ${description || ''}`;
+
+    fs.writeFile(path.join(FILES_DIR, fileName), content, err => {
+        if (err) {
+            return res.status(500).send('Failed to create todo');
+        }
+        res.redirect('/');
+    });
+});
+
+/* Server */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ public/javascripts/script.js
@@ -0,0 +1,7 @@
+
+    const h1=document.querySelector('h1');
+    h1.style.color='blue';
+    h1.style.backgroundColor='lightgray';
+    h1.style.padding='10px';
+    h1.style.textAlign='center';
+    
